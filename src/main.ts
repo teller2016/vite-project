6854c967c3c548e0916d53b9305eeb33ce081b2b
teller2016 <teller75@naver.com>
2023-10-30 13:45:57 +0900

    feat: router added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createSSRApp } from "vue";
 import App from './App.vue';
+import { createRouter } from './router';
 
 export function createApp() {
     const app = createSSRApp(App);
-    return {app};
+
+    const router = createRouter();
+    app.use(router);
+
+    return {app, router};
 }
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Home</h1>
+    <p>
+        홈
+    </p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
